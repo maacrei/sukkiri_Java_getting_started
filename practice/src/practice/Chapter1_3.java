146bf6e8b1d868cee54a2fbbea065e05efae8154
神原 茉里 <maacrei5723@gmail.com>
2022-06-09 18:30:50 +0900

    Chapter3

---
+++ practice/src/practice/Chapter1_3.java
@@ -0,0 +1,12 @@
+package practice;
+
+public class Chapter1_3 {
+	public static void main(String[] args) {
+		boolean result = true;
+		char character = '駆';
+		double pi = 3.14;
+		long number = 3144159265853979L;
+		String attack = "ミナトの攻撃！敵に15ポイントのダメージを与えた。";
+		System.out.println("敵に遭遇した！" + " " + attack);
+	}
+}
---
+++ practice/src/practice/Chapter3_4.java
@@ -0,0 +1,14 @@
+package practice;
+
+public class Chapter3_4 {
+	public static void main(String[] args) {
+		boolean tenki = true;
+		if (tenki == true) {
+			System.out.println("洗濯をします");
+			System.out.println("散歩にいきます");
+		} else {
+			System.out.println("DVDを見ます");
+			System.out.println("寝ます");
+		}
+	}
+}
